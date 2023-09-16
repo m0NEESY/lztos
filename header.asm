ae3fecf4c3b31d254c4865db49c0e9c22c0f1cee
m0NEESY <130301235+m0NEESY@users.noreply.github.com>
2023-09-16 23:11:43 +0500

    Create header.asm

---
+++ header.asm
@@ -0,0 +1,17 @@
+section .multiboot_header
+header_start:
+    dd 0xe85250d6
+
+    dd 0
+
+    dd header_end - header_start
+
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    dw 5
+    dw 0
+    dd 20
+    dd 320
+    dd 200
+    dd 8
+header_end:
